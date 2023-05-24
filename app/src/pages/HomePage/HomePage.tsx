75ec766b6f0f153a5578464ffefd39baf26cc329
Romashkina <da_shootka@mail.ru>
2023-05-24 17:41:37 +0200

    feat: create app, add eslint and prettier

---
+++ app/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './HomePage.module.css';
+
+function HomePage() {
+  return (
+    <div className={styles.container}>
+      <p>game</p>
+      <p>top 10</p>
+    </div>
+  );
+}
+
+export default HomePage;
